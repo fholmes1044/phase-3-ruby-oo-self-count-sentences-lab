b1118f2c7d9966f9d5ede0bc5ac31d79400005df
+ <+>
2023-01-08 16:13:05 -0500

    commit tree 07b0391fee57d11c21fd6f4b9685144a82e648f3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    delimiters = ['!','!!' '?', "."]
+    split_self = self.split(Regexp.union(delimiters))
+     puts split_self.length
+     puts split_self
+    count = split_self.count
 
   end
 end
